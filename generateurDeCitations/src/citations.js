4b52348a1e8c000cdb87e269cd6312b444254385
ImMyst <titouan.galvani@laposte.net>
2018-05-24 11:28:49 +0200

    files added

---
+++ generateurDeCitations/src/citations.js
@@ -0,0 +1,42 @@
+module.exports = {
+  citation1: {
+    auteur: ' Philippe Naszályi',
+    citation: 'Quand le sage désigne la lune, l’idiot regarde le doigt',
+   },
+  citation2: {
+    auteur: 'Seth Gueko',
+    citation: 'Suis-je métrosexuel si j’encule un contrôleur ?',
+  },
+  citation3: {
+    auteur: 'Seth Gueko',
+    citation: 'Dis pas à un zoophile que t’as un chat dans la gorge. ',
+  },
+  citation4: {
+    auteur: 'La Fouine',
+    citation: 'À trop haïr les noirs, Le Pen fait des nuits blanches.',
+  },
+  citation5: {
+    auteur: 'Seth Gueko',
+    citation: 'J’ai tatoué courage sur ma queue car je prends mon courage à deux mains.',
+  },
+  citation6: {
+    auteur: 'Le Maître Gims',
+    citation: 'J’ai deux tours d’avance, comme Mister Ben Laden.',
+  },
+  citation7: {
+    auteur: 'Jazzy Bazz',
+    citation: 'T’es plus zoophile que celui qui a découvert le lait de vache.',
+  },
+  citation8: {
+    auteur: 'Youssoupha',
+    citation: 'On te baise avec un sourire, et c’est la sodomie sponsorisé par Colgate.',
+  },
+  citation9: {
+    auteur: 'Volts Face',
+    citation: 'Je suis pas Naruto mais quand je te baise, t’as l’impression que j’ai neuf queues.',
+  },
+  citation10: {
+    auteur: 'Seth Gueko',
+    citation: 'J’pisse du champagne, viens boire à la bouteille.',
+  }
+};
