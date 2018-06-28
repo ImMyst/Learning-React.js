4997c31b0c158ccce4c5c3a006924e4989613487
Titouan <titouan.galvani@laposte.net>
2018-06-29 00:40:38 +0200

    Adding new citations :pencil: | Correct intends according to coding standards :ok_hand:

--- src/index.js
+++ src/index.js
@@ -7,31 +7,31 @@ import './index.css';
 
 class App extends React.Component {
 
-  state = {};
+    state = {};
 
-  genererCitation = event => {
-    // Translate citates in array
-    const keyArray = Object.keys(citations);
-    // Random citate
-    const randomKey = keyArray[Math.floor(Math.random() * keyArray.
-    length)];
-    this.setState(citations[randomKey]);
-  };
+    genererCitation = event => {
+        // Translate citates in array
+        const keyArray = Object.keys(citations);
+        // Random citate
+        const randomKey = keyArray[Math.floor(Math.random() * keyArray.
+            length)];
+        this.setState(citations[randomKey]);
+    };
 
-  render() {
-    return (
-      <div>
-        <p>
-          {this.state.citation}
-          <span>- {this.state.auteur}</span>
-        </p>
-        <button onClick={e => this.genererCitation(e)}>Une autre citation!</button>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <p>
+                    {this.state.citation}
+                    <span>- {this.state.auteur}</span>
+                </p>
+                <button onClick={e => this.genererCitation(e)}>Une autre citation!</button>
+            </div>
+        )
+    }
 }
 
 render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
