cb0dc61b071d1bf56e3aa54c829eda3a2cda3cec
Titouan <titouan.galvani@laposte.net>
2018-07-01 03:26:58 +0200

    New indentation :wrench: with components directory

--- src/index.js
+++ src/index.js
@@ -1,42 +1,7 @@
-// Code React
-
 import React from 'react';
 import { render } from 'react-dom';
-import citations from './citations';
+import App from './components/App';
 import './index.css';
-
-class App extends React.Component {
-
-    state = {};
-
-    componentWillMount() {
-        this.genererCitation();
-    }
-
-    genererCitation = event => {
-        // Translate citation in array
-        const keyArray = Object.keys(citations);
-        // Random citation
-        const randomKey = keyArray[Math.floor(Math.random() * keyArray.length)];
-        if (this.state.citation === citations[randomKey].citation) {
-            this.genererCitation();
-            return
-        }
-        this.setState(citations[randomKey]);
-    };
-
-    render() {
-        return (
-            <div>
-                <p>
-                    {this.state.citation}
-                    <span>- {this.state.auteur}</span>
-                </p>
-                <button onClick={e => this.genererCitation(e)}>Once agaaain !</button>
-            </div>
-        )
-    }
-}
 
 render(
     <App />,
